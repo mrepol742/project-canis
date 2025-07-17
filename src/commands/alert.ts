1b2928b41db84b18ddf3b2e883a1769635d16ba9
Melvin Jones Repol <mrepol742@gmail.com>
2025-07-18 01:49:36 +0800

    added popcat commands

---
+++ src/commands/alert.ts
@@ -0,0 +1,36 @@
+import { Message, MessageMedia } from "whatsapp-web.js";
+import axios from "axios";
+import log from "npmlog";
+import fs from "fs/promises";
+import { client } from "../index";
+
+export const command = "alert";
+export const role = "user";
+
+export default async function (msg: Message) {
+  const query = msg.body.replace(/^alert\b\s*/i, "").trim();
+  if (query.length === 0) {
+    await msg.reply("Please provide a text.");
+    return;
+  }
+
+  await axios
+    .get(`https://api.popcat.xyz/alert?text=${encodeURIComponent(query)}`, {
+      responseType: "arraybuffer",
+    })
+    .then(async (response) => {
+      const tempDir = "./.temp";
+      await fs.mkdir(tempDir, { recursive: true });
+
+      const tempPath = `${tempDir}/${Date.now()}.png`;
+      await fs.writeFile(tempPath, response.data);
+
+      const media = MessageMedia.fromFilePath(tempPath);
+      await msg.reply(media);
+      await fs.unlink(tempPath);
+    })
+    .catch(async (error) => {
+      log.error("alert", `Error fetching image: ${error.message}`);
+      await msg.reply("Error fetching image. Please try again later.");
+    });
+}
