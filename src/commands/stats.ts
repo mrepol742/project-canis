0c4a052a38184a4f36f36b3d9a424e1da4eb8ab0
Melvin Jones Repol <mrepol742@gmail.com>
2025-07-18 08:20:06 +0800

    update stats command

--- src/commands/stats.ts
+++ src/commands/stats.ts
@@ -1,31 +1,36 @@
 import { Message } from "whatsapp-web.js";
 import log from "npmlog";
 import os from "os";
-import { name, version } from "../../package.json";
+import si from "systeminformation";
 
 export const command = "stats";
 export const role = "user";
 
 export default async function (msg: Message) {
   const stats = {
-    platform: os.platform(),
+    OS: os.release(),
     arch: os.arch(),
-    cpuCount: os.cpus().length,
+    cpu: os.cpus(),
+    usedMemory: os.totalmem() - os.freemem(),
     totalMemory: os.totalmem(),
-    freeMemory: os.freemem(),
-    uptime: process.uptime(),
+    uptime: process.uptime()
   };
+  
+  const gpuInfo = await si.graphics();
+  const osInfo = await si.osInfo();
 
   const statsMessage = `
       *System Stats:*
-      - Platform: ${stats.platform}
-      - Architecture: ${stats.arch}
-      - CPU Count: ${stats.cpuCount}
-      - Total Memory: ${(stats.totalMemory / 1024 ** 3).toFixed(2)} GB
-      - Free Memory: ${(stats.freeMemory / 1024 ** 3).toFixed(2)} GB
+
+      - OS: ${osInfo.distro} ${osInfo.kernel}
+      - CPU: ${stats.cpu[0].model}
+      - GPU: ${gpuInfo.controllers.map(c => c.model).join(", ")}
+      - RAM: ${(stats.usedMemory / (1024 ** 3)).toFixed(2)} GB / ${(stats.totalMemory / (1024 ** 3)).toFixed(2)} GB
+      - VRam: ${gpuInfo.controllers.map(c => c.vram).join(", ")}
       - Uptime: ${(stats.uptime / 3600).toFixed(2)} hours
-      - Node.js Version: ${process.version}
-      - ${name}: ${version}
+      - Load Avg: ${os.loadavg().map(n => n.toFixed(2)).join(", ")}
+      - Process: #${process.pid} ${process.title}
+      - Node.js: ${process.version}
     `;
 
   await msg.reply(statsMessage);
