3b049dbcb19f31f80e91317067730878bcfa8368
Melvin Jones Repol <mrepol742@gmail.com>
2025-07-22 08:08:27 +0800

    feat: add legal commands for DCMA, privacy, and terms of service

---
+++ src/commands/legal.ts
@@ -0,0 +1,33 @@
+import { Message } from "whatsapp-web.js";
+import log from "../components/utils/log";
+
+export const info = {
+  command: "legal",
+  description: "List down legal commands.",
+  usage: "legal",
+  example: "legal",
+  role: "user",
+  cooldown: 5000,
+};
+
+export default async function (msg: Message) {
+    if (!/^legal$/i.test(msg.body)) return;
+    
+    const text = `
+    \`Legal Commands\`
+
+       \`terms\`
+       Display the terms of service of the bot.
+
+       \`privacy\`
+       Display the privacy policy of the bot.
+
+       \`dcma\`
+       Display the DCMA policy of the bot.
+
+       \`contact\`
+       mrepol742@gmail.com
+    `;
+
+    await msg.reply(text);
+}
