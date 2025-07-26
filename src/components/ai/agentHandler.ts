e79c19e443e259b00e0cdddf0b415983a5aeeab8
Melvin Jones Repol <mrepol742@gmail.com>
2025-07-26 18:19:54 +0800

    feat: add support for Gemini AI provider; update agent handler and environment configuration

--- src/components/ai/agentHandler.ts
+++ src/components/ai/agentHandler.ts
@@ -1,5 +1,6 @@
 import { openrouter, generateText } from "./openRouter";
 import { groq } from "./groq";
+import { gemini } from "./gemini";
 import redis from "../redis";
 
 const aiProvider = process.env.AI_PROVIDER || "groq";
@@ -9,6 +10,11 @@ const queryCachingCount = parseInt(
   10
 );
 const queryCachingTTL = parseInt(process.env.QUERY_CACHING_TTL || "3600", 10);
+const openRouterModel =
+  process.env.OPEN_ROUTER_MODEL || "moonshotai/kimi-k2:free";
+const groqModel =
+  process.env.GROQ_MODEL || "meta-llama/llama-4-scout-17b-16e-instruct";
+const geminiModel = process.env.GEMINI_MODEL || "gemini-2.0-flash-001";
 
 function getCacheKey(prompt: string) {
   return `ai:prompt:${Buffer.from(prompt).toString("base64")}`;
@@ -25,18 +31,42 @@ export default async function (prompt: string, model?: string) {
   }
 
   let result: string | null;
+  /*
+   * OpenRouter
+   * https://openrouter.ai/docs/api-reference
+   */
   if (aiProvider === "openrouter") {
     const { text } = await generateText({
-      model: openrouter(model || "moonshotai/kimi-k2:free"),
+      model: openrouter(model || openRouterModel),
       prompt: prompt,
     });
     result = text;
+
+    /*
+     * Groq
+     * https://console.groq.com/docs/api-reference
+     */
   } else if (aiProvider === "groq") {
     const chatCompletion = await groq.chat.completions.create({
       messages: [{ role: "user", content: prompt }],
-      model: model || "meta-llama/llama-4-scout-17b-16e-instruct",
+      model: model || groqModel,
     });
     result = chatCompletion.choices[0].message.content;
+
+    /*
+     * Gemini
+     * https://github.com/googleapis/js-genai
+     */
+  } else if (aiProvider === "gemini") {
+    const generateContent = await gemini.models.generateContent({
+      model: model || geminiModel,
+      contents: prompt,
+    });
+    result = generateContent.text || null;
+
+    /*
+     * Error handling for unsupported AI providers
+     */
   } else {
     throw new Error(`Unsupported AI provider: ${aiProvider}`);
   }
