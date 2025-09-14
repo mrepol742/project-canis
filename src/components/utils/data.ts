4d4e785aaf3c92e0644101eb1df7a1d573881394
Melvin Jones Repol <mrepol742@gmail.com>
2025-09-14 10:07:31 +0800

    feat: added cli-progress to loading of data

--- src/components/utils/data.ts
+++ src/components/utils/data.ts
@@ -1,22 +1,45 @@
 import fs from "fs";
 import log from "./log";
 import path from "path";
+import cliProgress from "cli-progress";
 
 function safeReadJSON(filePath: string) {
   try {
-    return JSON.parse(fs.readFileSync(path.resolve(__dirname, filePath), "utf-8"));
+    return JSON.parse(
+      fs.readFileSync(path.resolve(__dirname, filePath), "utf-8")
+    );
   } catch (error) {
     log.error("Data", `Failed to read JSON from ${filePath}`, error);
     return [];
   }
 }
 
-const greetings = safeReadJSON("../../data/greetings.json");
-const ball = safeReadJSON("../../data/8ball.json");
-const cat = safeReadJSON("../../data/cat.json");
-const dyk = safeReadJSON("../../data/dyk.json");
-const joke = safeReadJSON("../../data/joke.json");
-const quiz = safeReadJSON("../../data/quiz.json");
-const wyr = safeReadJSON("../../data/wyr.json");
+const files: Record<string, string> = {
+  greetings: "../../data/greetings.json",
+  ball: "../../data/8ball.json",
+  cat: "../../data/cat.json",
+  dyk: "../../data/dyk.json",
+  joke: "../../data/joke.json",
+  quiz: "../../data/quiz.json",
+  wyr: "../../data/wyr.json",
+};
 
-export { ball, cat, dyk, joke, quiz, wyr, greetings };
+const progressBar = new cliProgress.SingleBar(
+  {
+    format: "Loading Data [{bar}] {percentage}% | {value}/{total} files",
+  },
+  cliProgress.Presets.shades_classic
+);
+
+progressBar.start(Object.keys(files).length, 0);
+
+const data: Record<string, any> = {};
+for (const [key, filePath] of Object.entries(files)) {
+  data[key] = safeReadJSON(filePath);
+  progressBar.increment();
+}
+
+progressBar.stop();
+
+const { greetings, ball, cat, dyk, joke, quiz, wyr } = data;
+export { greetings, ball, cat, dyk, joke, quiz, wyr };
