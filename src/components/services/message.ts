9985f16cebadefe4735398e45ebaaba89b2c1438
Melvin Jones Repol <mrepol742@gmail.com>
2025-09-14 09:01:31 +0800

    fix: the provided value in body content is too long for the col type.

--- src/components/services/message.ts
+++ src/components/services/message.ts
@@ -2,18 +2,26 @@ import { prisma } from "../prisma";
 import log from "../../components/utils/log";
 import { Message } from "whatsapp-web.js";
 
+const MAX_LENGTH = 191;
+
+function filterContent(body: string): string {
+  if (body.length <= MAX_LENGTH) return body;
+
+  const cutoff = MAX_LENGTH - " [REDACTED]".length;
+  return body.slice(0, cutoff) + " [REDACTED]";
+}
+
 export async function addMessage(
   msg: Message,
   body: string,
-  type: string
+  type: string,
 ): Promise<void> {
   try {
-
     const lid = msg.id.remote.split("@")[0];
     await prisma.message.create({
       data: {
         lid,
-        content: body,
+        content: filterContent(body),
         type: type,
       },
     });
