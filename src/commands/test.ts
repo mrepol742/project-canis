0f5792e83f23db17196dc0179272cca423f83c52
Melvin Jones Repol <mrepol742@gmail.com>
2025-10-18 23:13:14 +0800

    feat: add --error in test and make it admin only

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,15 +1,23 @@
-import { Message } from "../types/message"
+import { Message } from "../types/message";
 
 export const info = {
   command: "test",
-  description: "A simple test command.",
-  usage: "test",
-  example: "test",
-  role: "user",
+  description:
+    "A simple test command with optional error to test error handling.",
+  usage: "test [--error]",
+  example: "test --error",
+  role: "admin",
   cooldown: 5000,
 };
 
 export default async function (msg: Message): Promise<void> {
+  const match = /^test(?:\s+--error)?$/i.exec(msg.body.trim());
+  if (!match) return;
+
+  const error = msg.body.includes("--error");
+
+  if (error) throw Error("This is a test error");
+
   const testMessage = `
     \`Hello World\`
 
