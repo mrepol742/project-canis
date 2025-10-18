4429329eb6b218aa459988b280ef45a10e425d53
Melvin Jones Repol <mrepol742@gmail.com>
2025-10-19 00:29:37 +0800

    chore: simplify logging in production no stracktrace

--- src/components/utils/log.ts
+++ src/components/utils/log.ts
@@ -1,5 +1,22 @@
 import log from "npmlog";
-import fs from "fs";
-import path from "path";
+
+if (process.env.DEBUG === "false") {
+  const methodsToPatch = ["error", "warn"];
+
+  for (const method of methodsToPatch) {
+    const original = log[method].bind(log);
+
+    log[method] = function (prefix: string, ...args: any[]) {
+      const formattedArgs = args.map((arg) => {
+        if (arg instanceof Error) {
+          return `${arg.name}: ${arg.message}`;
+        }
+        return arg;
+      });
+
+      return original(prefix, ...formattedArgs);
+    };
+  }
+}
 
 export default log;
