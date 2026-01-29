9add59380af81847bf98b1e66f7ea8ca85335f3a
Melvin Jones Repol <mrepol742@gmail.com>
2026-01-29 12:19:13 +0800

    feat: add landing page
    - Serve src/components/web/index.html for HTTP requests
    - Add fs and path imports and send HTML with proper content-type
    - Add Tailwind-based landing page at src/components/web/index.html
    - Return 500 on file read errors

--- src/components/server.ts
+++ src/components/server.ts
@@ -1,14 +1,26 @@
 import http from "http";
 import log from "./utils/log";
 import * as Sentry from "@sentry/node";
+import fs from "fs";
+import path from "path";
 import { PORT } from "../config";
 
 const MAX_PORT_TRIES = 10;
 
 function startServer(port: number, tries = 0) {
   const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Bot is running\n");
+    const filePath = path.join(__dirname, "web", "index.html");
+
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+        return;
+      }
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
   });
 
   server.listen(port, () => {
